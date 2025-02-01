95da6df718ce787be1e58a13803bcde161d138b4
David Fink <davidfink3@gmail.com>
2025-02-01 14:12:18 -0500

    Pushing

--- server/app.js
+++ server/app.js
@@ -54,5 +54,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8333;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
